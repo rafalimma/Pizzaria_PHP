42461b6baf16059cb84f65fd34af9d007df1fcd4
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-10-25 16:36:00 -0300

    css da dasboard

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,85 @@
 <?php
     include_once("php_templates/header.php");
 ?>
-    <h1> Dashboard </h1>
+
+    <style>
+        .delete-btn, .update-btn {
+        border: none;
+        background-color: transparent;
+        }
+        .delete-btn {
+        padding-top: 10px;
+        }
+        .delete-btn i {
+        color: red;
+        font-size: 20px;
+        }
+        .update-btn i {
+        color: #004085;
+        }
+        .update-form {
+        display: flex;
+        flex-direction: row;
+        }
+        .status-input {
+            width: 65%;
+        }
+        .table {
+            text-align: left;
+        }
+    </style>
+    <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Gerenciar Pedidos:</h2>
+                </div>
+                <div class="col-md-12 table-container">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col"><span>Pedido</span>#</th>
+                                <th scope="col"><span>Borda</span>#</th>
+                                <th scope="col"><span>Massa</span>#</th>
+                                <th scope="col"><span>Sabores</span>#</th>
+                                <th scope="col"><span>Ações</span>#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>Cheddar</td>
+                                <td>Catupiry</td>
+                                <td>4 Queijos</td>
+                                <td>
+                                    <form action="core/order.php" method="POST" class="form-group update-form">
+                                        <input type="hidden" name="type" value="update">
+                                        <input type="hidden" name="id" value="1">
+                                        <select name="status" class="form-control">
+                                            <option value="">Entrega</option>
+                                        </select>
+                                        <button type="submit" class="update-btn">
+                                            <i class="fas fa-sync-alt"></i>
+                                        </button>
+                                    </form>
+
+                                </td>
+                                <td>
+                                    <form action="core/orders.php" method="POST">
+                                        <input type="hidden" name="type" value="delete">
+                                        <input type="hidden" name="id" value="1">
+                                        <button type="submit" class="update-btn">
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
     include_once("php_templates/footer.php");
 ?>
--- php_templates/header.php
+++ php_templates/header.php
@@ -17,9 +17,9 @@ if(isset($_SESSION["msg"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Faça seu pedido</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
